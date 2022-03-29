d0498cb946179f13fabc0a4e93661a3ce5fe89a3
bbangaro <bbangaro@gmail.com>
2022-03-29 11:52:02 +0900

    [update]비디오 이미지 세팅하기

--- js/newMain.js
+++ js/newMain.js
@@ -16,8 +16,13 @@
                 messageB: document.querySelector('#scroll-section-0 .main-message.b'),
                 messageC: document.querySelector('#scroll-section-0 .main-message.c'),
                 messageD: document.querySelector('#scroll-section-0 .main-message.d'),
+                canvas: document.querySelector('#video-canvas-0'),
+                context: document.querySelector('#video-canvas-0').getContext('2d'),
+                videoImages: [],
             },
             values: {
+                videoImageCount: 300,
+                imageSequence: [0, 299],
                 messageA_opacity_in: [0, 1, { start: 0.1, end: 0.2 }],
                 messageB_opacity_in: [0, 1, { start: 0.3, end: 0.4 }],
                 messageC_opacity_in: [0, 1, { start: 0.5, end: 0.6 }],
@@ -88,6 +93,17 @@
             values: {},
         },
     ];
+
+    // videoImages에 이미지들 넣어주기
+    function setCanvasImages() {
+        let imgElem;
+        for (let i = 0; i < sceneInfo[0].values.videoImageCount; i++) {
+            imgElem = new Image(); // 이미지 객체 생성
+            imgElem.src = `./video/001/IMG_${6726 + i}.JPG`;
+            sceneInfo[0].objs.videoImages.push(imgElem);
+        }
+    }
+    setCanvasImages();
 
     function setLayout() {
         // 각 스크롤 섹션 높이 세팅
